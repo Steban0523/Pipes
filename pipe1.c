3d5bde7a61dadb5ee75531d4e51bd458866edd17
Steban0523 <96693259+Steban0523@users.noreply.github.com>
2022-01-19 22:19:35 -0500

    Create pipe1.c

---
+++ pipe1.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define READ  0
+#define WRITE 1
+
+int main() {
+
+  pid_t pid;
+  int   fd[2];
+  char*const tx0[] = {"sort",NULL};
+  char*const tx[] = {"cat","/etc/hosts",NULL};
+
+  if (pipe(fd) == -1) {
+	perror("Creating pipe");
+	exit(EXIT_FAILURE);
+  }
+
+  pid = fork();
+  if (pid == -1) {
+	perror("fork() failed)");
+	exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+	// The child process will execute wc.
+	// Close the pipe write descriptor.
+	close(fd[WRITE]);
+	// Redirect STDIN to read from the pipe.
+	dup2(fd[READ], STDIN_FILENO);
+	// Execute wc
+	execvp(tx0[0], tx0);
+  } else {
+	// The parent process will execute ls.
+	// Close the pipe read descriptor.
+	close(fd[READ]);
+	// Redirect STDOUT to write to the pipe.
+	dup2(fd[WRITE], STDOUT_FILENO);
+	// Execute ls -l
+	execvp(tx[0], tx);
+  }
+}
